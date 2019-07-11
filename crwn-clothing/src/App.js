8e930d6e126ab5497a39cca5a2cc416ed4362788
Keith <keiththex@yahoo.com>
2019-07-10 18:21:59 -0700

    CRA init, added homepage files

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HomePage from './homepage.component'
+
+
+function App() {
+  return (
+    <div>
+      Hello World
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
