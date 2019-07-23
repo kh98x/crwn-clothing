cbc19cf18dcd75ed24c4fadc4230e0c6c7ec96a8
Keith <keiththex@yahoo.com>
2019-07-22 20:44:32 -0700

    Refactored code with redux saga middleware

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,8 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
 
-import { auth, signInWithGoogle } from '../../components/firebase/firebase.utils'
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
+import { connect } from 'react-redux';
 
 class SignIn extends React.Component {
     constructor(props){
@@ -18,14 +19,10 @@ class SignIn extends React.Component {
     handleSubmit = async (e) => {
         e.preventDefault();
 
+        const { emailSignInStart } = this.props
         const { email, password } = this.state
 
-        try {
-            await auth.signInWithEmailAndPassword(email, password)
-            this.setState({email: '', password: ''})
-        } catch (e) {
-            console.log(e)
-        }
+        emailSignInStart(email, password)
 
     }
 
@@ -36,6 +33,8 @@ class SignIn extends React.Component {
     }
 
     render(){
+        const { googleSignInStart } = this.props
+
         return (
             <div className="sign-in" onSubmit={this.handleSubmit}>
                 <h2>I already have an account</h2>
@@ -57,7 +56,11 @@ class SignIn extends React.Component {
                     />
                     <div className="buttons">
                         <CustomButton type="submit">Sign in</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton 
+                            type="button"
+                            onClick={googleSignInStart}
+                            isGoogleSignIn
+                        >
                             Sign in with Google
                         </CustomButton> 
                     </div>
@@ -67,4 +70,9 @@ class SignIn extends React.Component {
     }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => dispatch(emailSignInStart({ email, password }))
+})
+
+export default connect(null, mapDispatchToProps)(SignIn);
--- src/pages/shoppage/shop.component.jsx
+++ src/pages/shoppage/shop.component.jsx
@@ -4,12 +4,12 @@ import './shop.styles.scss';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
 import CollectionPageContainer from '../collection/collection.container';
 import { connect } from 'react-redux';
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions'
 
 class ShopPage extends React.Component{
     componentDidMount(){
-        const { fetchCollectionsStartAsync } = this.props;
-        fetchCollectionsStartAsync();
+        const { fetchCollectionsStart } = this.props;
+        fetchCollectionsStart();
     }
 
     render(){
@@ -28,7 +28,7 @@ class ShopPage extends React.Component{
 }    
 
 const mapDispatchToProps = (dispatch) => ({
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage);
